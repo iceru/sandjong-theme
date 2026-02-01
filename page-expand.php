190598e14ec47847d8aa56bcbe3b73d3dc79de87
Muhamad Hafiz <m.hafiz1825@gmail.com>
2026-02-01 09:25:45 +0700

    feat: expand page

---
+++ page-expand.php
@@ -0,0 +1,130 @@
+<?php get_header(); ?>
+
+<section class="relative h-[587px]">
+    <div class="w-full h-[110%] left-0 -top-[10%] absolute" data-scroll data-scroll-speed="2">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/expand/hero.png" class="w-full object-cover h-full"
+            alt="">
+    </div>
+    <div class="container flex flex-col justify-end h-full pb-28 relative z-10 w-full">
+        <div class="grid grid-cols-2 w-full">
+            <h4 class="text-white w-[366px]">A partnership rooted in care, guided by heritage</h4>
+            <p class="text-white w-[591px]">
+                In the world of wellness, what is genuine always moves gently from within. Through our
+                Spa Operation and Franchise Partnerships, Sandjong welcomes you to build spaces that honor tradition
+                while embracing the needs of today. With the guidance of Amerta Jiwa Indonesia, we offer cultural depth,
+                thoughtful service, and seasoned expertise to help you cultivate a sanctuary of your own, in any place
+                you choose.
+            </p>
+        </div>
+    </div>
+</section>
+
+<section class="bg-beige py-16 relative z-10">
+    <div class="container grid grid-cols-2">
+        <div>
+            <h4 class="text-terracota mb-6">
+                Spa Operation
+            </h4>
+            <p class="body mb-16">
+                Let us carry the rhythm of Sandjong into your space.
+            </p>
+            <p class="body max-w-[440px]">
+                Bringing a spa to life requires more than a good idea, as it takes guidance, systems, and soul. At
+                Sandjong, through the expertise of Amerta Jiwa Indonesia, we offer a full suite of operational services
+                to help you create or elevate your wellness experience.
+                <br /> <br />
+                Whether you're a hotel, residence, or lifestyle destination, we offer complete support: from concept to
+                treatment, from therapist to guest. Every detail is shaped with intention, designed to reflect the
+                essence of Sandjong, where healing is deeply rooted in tradition.
+
+            </p>
+        </div>
+        <div>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/expand/spa-operation.png" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="bg-primary py-20 relative z-10">
+    <div class="absolute w-full h-full left-0 top-0">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/bg-green.jpg" class="w-full h-full object-cover"
+            alt="">
+    </div>
+    <div class="container relative z-10">
+        <h3 class="text-gold mb-11">Our Services</h3>
+        <div class="grid md:grid-cols-2 gap-32">
+            <div>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/expand/our-services.png" class="" alt="">
+            </div>
+            <div>
+                <div class="flex pb-5 border-b border-beige space-x-16 text-beige mb-16">
+                    <div>
+                        <h5>
+                            Consulting
+                        </h5>
+                    </div>
+                    <div>
+                        <h5>
+                            Training
+                        </h5>
+                    </div>
+                    <div>
+                        <h5>
+                            Management
+                        </h5>
+                    </div>
+                </div>
+                <div>
+                    <h4 class="text-gold mb-4">
+                        Wellness Consulting
+                    </h4>
+                    <p class="body text-beige mb-16">
+                        Shaping wellness with insight and care.
+                    </p>
+                    <p class="body text-beige max-w-[430px]">
+                        We guide and work closely with property owners, hoteliers, and wellness entrepreneurs to shape
+                        spas with depth and intention. Our support covers concepting, spatial design, product curation,
+                        and the full guest experience.
+                        <br /><br />
+                        And because wellness is more than a facility, we help integrate it
+                        into your property's culture, creating programs that feel thoughtful, true, and naturally
+                        connected to their setting.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="relative">
+    <div class="absolute w-full h-[130%] left-0 -top-[20%]" data-scroll data-scroll-speed="2">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/bg-timeline.png" class="w-full h-full object-cover"
+            alt="">
+    </div>1
+    <div class="container flex flex-col justify-center items-center text-center py-36 relative z-10">
+        <h3 class="text-gold mb-4">For Franchise</h3>
+        <p class="text-beige body mb-9">
+            Own a Sandjong sanctuary, guided by our legacy.
+        </p>
+        <p class="body text-beige max-w-[611px]">
+            We offer the opportunity to open a Sandjong spa through a boutique franchise model that’s designed for those
+            who believe in the power of culture-based wellness and wish to bring it to new communities.
+        </p>
+    </div>
+</section>
+
+<section class="bg-beige py-32 container flex">
+    <div>
+        <p class="text-primary max-w-[326px]">
+            Each Sandjong franchise is thoughtfully curated: maintaining our standards of care, heritage, and sensory
+            experience, while allowing your space to reflect the spirit of its surroundings.
+        </p>
+    </div>
+    <div class="">
+        <p class="text-primary">You will receive:</p>
+        <div class="grid md:grid-cols-3">
+
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
