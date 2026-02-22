d854a8810c279fd72867da447a45696e766d1e9a
Muhamad Hafiz <m.hafiz1825@gmail.com>
2026-02-22 23:13:36 +0700

    update image

--- page-expand.php
+++ page-expand.php
@@ -73,13 +73,13 @@
         ));
 
         if ($services->have_posts()):
-            ?>
+        ?>
             <div class="grid md:grid-cols-2 gap-14 md:gap-32">
                 <div class="relative">
                     <?php while ($services->have_posts()):
                         $services->the_post();
                         $activeClass = ($services->current_post === 0) ? '' : 'hidden'; // First visible, others hidden
-                        ?>
+                    ?>
                         <img src="<?php echo get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/images/expand/our-services.webp'; ?>"
                             class="service-image masked-facility w-full h-auto object-cover <?php echo $activeClass; ?>"
                             data-id="service-<?php echo get_the_ID(); ?>" alt="<?php the_title(); ?>">
@@ -92,7 +92,7 @@
                         <?php while ($services->have_posts()):
                             $services->the_post();
                             $opacityClass = ($services->current_post === 0) ? 'opacity-100 border-b-white' : 'opacity-40 border-b-transparent';
-                            ?>
+                        ?>
                             <div class="cursor-pointer pb-5 border-b-2 border-transparent service-tab transition-opacity duration-300 <?php echo $opacityClass; ?>"
                                 data-target="service-<?php echo get_the_ID(); ?>">
                                 <h5 class="whitespace-nowrap">
@@ -106,7 +106,7 @@
                         <?php while ($services->have_posts()):
                             $services->the_post();
                             $activeClass = ($services->current_post === 0) ? '' : 'hidden';
-                            ?>
+                        ?>
                             <div class="service-content <?php echo $activeClass; ?>" id="service-<?php echo get_the_ID(); ?>">
                                 <h4 class="text-gold mb-2">
                                     <?php the_title(); ?>
@@ -122,12 +122,13 @@
                     </div>
                 </div>
             </div>
-            <?php wp_reset_postdata(); endif; ?>
+        <?php wp_reset_postdata();
+        endif; ?>
     </div>
 </section>
 <script>
-    jQuery(document).ready(function ($) {
-        $('.service-tab').click(function () {
+    jQuery(document).ready(function($) {
+        $('.service-tab').click(function() {
             var targetId = $(this).data('target');
 
             // Handle Tabs
@@ -217,7 +218,7 @@
                 </div>
                 <div>
                     <div class="w-16 h-16 mb-9 rounded-full bg-gold/20 flex justify-center items-center">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/icons/guarantee.png"
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/icons/guarantee-2s.png"
                             class="w-[42px] h-[42px] object-contain" alt="">
                     </div>
                     <h5 class="text-primary max-w-[150px] md:max-w-[210px]">Continuous operational consulting and
